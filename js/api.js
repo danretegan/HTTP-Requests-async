fe82f54674f1bb7407cbe2bf8cbe2d85e075291b
Dan <danretegan@yahoo.com>
2023-11-24 17:12:36 +0200

    api.js & script.js completed

--- js/api.js
+++ js/api.js
@@ -2,3 +2,27 @@ import { updateNewsList } from "./markup.js";
 
 const ENDPOINT = "https://newsapi.org/v2/everything";
 const API_KEY = "af0e766a1ccf475588965f28a4346fe4";
+
+// https://newsapi.org/v2/everything?q=tesla&from=2023-10-24&sortBy=publishedAt&apiKey=API_KEY
+
+const getNews = async (query) => {
+  try {
+    const rezultat = await fetch(`${ENDPOINT}?q=${query}&apiKey=${API_KEY}`);
+    console.dir(rezultat);
+    const raspuns = await rezultat.json();
+    console.dir(raspuns);
+
+    return raspuns.articles;
+  } catch (eroarea) {
+    updateNewsList(`<p>${eroarea}</p>`);
+    console.error(eroarea);
+  }
+};
+
+// Axios:
+// async function getNews(query) {
+//     const raspuns = await axios.get(`${ENDPOINT}?q=${query}&apiKey=${API_KEY}`);
+//     return raspuns.data.articles;
+// }
+
+export default { getNews };
